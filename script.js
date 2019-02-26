13a0389f735098e6b5655edc9a3327a525d6223e
Aaron <aaron.botello.h@gmail.com>
2019-02-26 15:16:08 -0600

    extra info added to petsData array of objects

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var petsData = [
      {
     name: "Purrsloud",
     species: "Cat",
-    favFoods: ["wet food", "sea food", "<strong>any</strong> food"],
+    favFoods: ["Beef food", "sea food", "<strong>any</strong> food","cavage food"],
     birthYear: 2017,
     photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
   },
@@ -25,7 +25,7 @@ var petsData = [
 function age(birthYear){
     //birthYear  is relationated with the pet.birthYear
     //
-  let calculatedAge = new Date().getFullYear()- birthYear;
+  let calculatedAge = new Date().getFullYear() - birthYear;
     if(calculatedAge < 1){
         return calculatedAge = "baby"; 
     
@@ -38,12 +38,12 @@ function age(birthYear){
     
     //new Date().getFullYear(); native to javascript 
 }
-//The argument name in foods does not matter 
-function foods(foods){
+//The argument name in foods function does not matter 
+function foods(foodss){
     return `
 <h4> Favorite Foods </h4> 
 <ul class= "foods-list"> 
-${foods.map(food =>`<li> ${food}</li>`
+${foodss.map(food =>`<li> ${food}</li>`
 ).join('')}
 </ul> 
 
@@ -51,11 +51,12 @@ ${foods.map(food =>`<li> ${food}</li>`
 }
 //ES6 functionality added 
 //maps() generates a new array it also takes a function 
+//pet it's just an argument 
 function petTemplate(pet){
     return `
 <div class="animal">
 <img class="pet-photo" src="${pet.photo}">
-<h2 class ="pet-name"> ${pet.name}<span class="species">(${pet.species})</span></h2>
+<h2 class ="pet-name"> ${pet.name} <span class="species">(${pet.species})</span></h2>
 <p><strong>Age: </strong>${age(pet.birthYear)} </p>
 ${pet.favFoods ? foods(pet.favFoods)  : ''}
 
