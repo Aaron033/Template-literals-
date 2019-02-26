f59c9431407cae486c270fa20e804777cf9a8859
Aaron <aaron.botello.h@gmail.com>
2019-02-25 20:18:27 -0600

    age() added to the birthYear of the animal

--- script.js
+++ script.js
@@ -21,20 +21,28 @@ var petsData = [
         photo: "http://learnwebcode.github.io/json-example/images/cat-1.jpg"
     }
 ];
+//The name in the argument does not matter, I just put the birthYear parameters because it will make more sense.
+function age(birthYear){
+    //birthYear  is relationated with the pet.birthYear
+    return new Date().getFullYear()- birthYear;
+    
+    //new Date().getFullYear(); native to javascript 
+}
 
 function petTemplate(pet){
     return `
 <div class="animal">
 <img class="pet-photo" src="${pet.photo}">
 <h2 class ="pet-name"> ${pet.name}<span class="species">(${pet.species})</span></h2>
+<p><strong>Age: </strong>${age(pet.birthYear)} </p>
 </div>
+
 `
 }
 
 
 document.getElementById("app").innerHTML = `
-<h1 class="app-title">Pets (${petsData.length} results)</h1>
-${petsData.map(petTemplate).join('')}
-
-<p class="footer">These ${petsData.length} pets were added recently. Check back soon for updates. </p> 
+ <h1 class="app-title">Pets (${petsData.length} results)</h1>
+ ${petsData.map(petTemplate).join('')}
+ <p class="footer">These ${petsData.length} pets were added recently. Check back soon for updates. </p> 
 `
